5796f683406fb655c99876eee5d3a4ce9a6a9807
Satoru <satoru@b-bump.net>
2021-05-08 07:50:02 -0700

    Create Project

---
+++ duckduckGo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  duckduckGo
+//
+//  Created by Satoru Ishii on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ duckduckGo/duckduckGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  duckduckGoApp.swift
+//  duckduckGo
+//
+//  Created by Satoru Ishii on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct duckduckGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
